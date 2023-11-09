e335b935955eaf05cf197563770f86627d152107
ksakthivel1211 <125632352+ksakthivel1211@users.noreply.github.com>
2023-11-10 00:09:23 +0530

    Message container was created

--- assignment-1/src/app/app.module.ts
+++ assignment-1/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MessageContainerComponent } from './message-container/message-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageContainerComponent
   ],
   imports: [
     BrowserModule
---
+++ assignment-1/src/app/message-container/message-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-container',
+  templateUrl: './message-container.component.html',
+  styleUrls: ['./message-container.component.css']
+})
+export class MessageContainerComponent {
+
+}
