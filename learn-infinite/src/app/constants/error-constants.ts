46b2abb3d285d12f333a31fbf9df76f9ce01da80
ksakthivel1211 <125632352+ksakthivel1211@users.noreply.github.com>
2023-12-19 11:46:04 +0530

    project removed

--- learn-infinite/src/app/constants/error-constants.ts
+++
@@ -1,2 +0,0 @@
-export const INVALID_COURSE_NAME = "Invalid restaurant name";
-export const INVALID_CREDENTIALS = "Invalid credentials";
--- learn-infinite/src/app/constants/image-constants.ts
+++
@@ -1,4 +0,0 @@
-export const LEARN_INFINITE_LOGO = "../../../assets/logos/Logo.png";
-export const CART_IMAGE = "../../../assets/icons/Image 10.png";
-export const SEARCH_ICON = "../../../assets/icons/magnifying-glass-solid.svg";
-export const PROFILE_ICON ="../../../assets/icons/profile.png";
